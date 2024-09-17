d684938a2eeb938228c4840b1b32261b9b4ee29e
John Vazquez <johnvazquez@knights.ucf.edu>
2024-09-17 02:27:12 -0400

    Created and styled contact page form, it is not complete, and recapatcha can not be put online until we have a domain and the website is launched. some additional styling edits will be needed, and a formspree connection will be added to make it work.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,5 @@
-window.addEventListener('scroll', function() {
-    var arrow = document.getElementById('scroll-arrow');
-    if (window.scrollY > 50) {
-        arrow.style.opacity = '0'; // Hide the arrow when scrolled down
-    } else {
-        arrow.style.opacity = '1'; // Show the arrow when at the top
-    }
-});
+function searchItems() {
+    const searchTerm = document.getElementById('search-input').value;
+    console.log('Searching for:', searchTerm);
+    // Future implementation: filter items based on searchTerm
+}
