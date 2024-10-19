601bf5312ab9380348aab1a02e8400462db8899c
Dhruv Sharma <vasusharma2000@hotmail.com>
2024-10-18 23:38:16 -0400

    Fixed issue with products/services page by merging my code into the master branch one

--- scripts.js
+++ scripts.js
@@ -3,3 +3,17 @@ function searchItems() {
     console.log('Searching for:', searchTerm);
     // Future implementation: filter items based on searchTerm
 }
+
+// button changes color from light grey to black and vice versa when clicked
+function buttonClicked(button) {
+    var button = document.getElementById("toggle-button");
+    
+    // toggle between light gray and black
+    if (button.style.backgroundColor === "rgb(204, 204, 204)") {
+        button.style.backgroundColor = "black";
+        button.innerHTML += " &#10004;";
+    } else {
+        button.style.backgroundColor = "#ccc";
+        button.innerHTML = button.innerHTML.replace(" &#10004;", "");
+    }
+}
